d28cdc7f9f5954f277536e8ab78d765d17dca423
Yuriy Mironov <git>
2017-08-15 17:05:24 +0400

    [*] пытаюсь прикрутить ангуляр

--- server.js
+++ server.js
@@ -2,18 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dbEngine from './dbengine';
 
+
 const app = express();
-const path = __dirname + '/views';
+app.use(express.static(__dirname + '/views'));
+
 
 app.use(bodyParser.json);
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
- app.get('/', async(req, res) => {
-     res.end('HW!');
- });
+app.get('*', async(req, res) => {
+    res.sendfile('./views/index.html');
+});
 
 
-app.listen(3000, err =>{
-    if(err) throw err;
+
+app.listen(3000, err => {
+    if (err) throw err;
     console.log('Сервер стартовал по адресу: http://localhost:3000');
-}); 
+});
---
+++ views/core.js
@@ -0,0 +1,5 @@
+// тут будет ангуляр
+import angular from 'angular';
+
+
+let zames = angular.module(zames, []);
